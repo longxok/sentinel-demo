5dff8d25024a65d1d21131995cb86eb6351a0313
vito <zhouwentao16@gmail.com>
2019-09-05 22:31:37 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Alibaba Sentinel限流Demo
 ## 启动项目
-启动项目后，访问地址：http://localhost:18084
+启动项目后，访问地址：http://localhost:18084/hello
 ## 启动Sentinel控制台
 下载地址：http://edas-public.oss-cn-hangzhou.aliyuncs.com/install_package/demo/sentinel-dashboard.jar  
 执行`java -jar sentinel-core-example.jar --server.port=18083`启动控制台
 ## 配置限流策略
-访问地址：http://localhost:18083 配置限流策略
+访问地址：http://localhost:18083 配置限流策略，用户名和密码都是`sentinel`
 ## 测试
 ...
 ## 参考
--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,16 +1,25 @@
 package com.example.demo;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class TestController {
 
     @GetMapping(value = "/hello")
-    @SentinelResource("hello")
+    @SentinelResource(value= "hello", blockHandler = "exceptionHandler")
     public String hello() {
         return "Hello Sentinel";
+    }
+
+    // 限流与阻塞处理
+    public void exceptionHandler(String str, BlockException ex) {
+        System.out.println("aaa");
+        log.error( "blockHandler：" + str, ex);
     }
 
 }
